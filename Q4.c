495773a38a033223832bcb45907a1ed0520786dd
JamesCook5 <james.cook24@students.bhasvic.ac.uk>
2024-10-24 11:53:43 +0100

    some work on Q4

---
+++ Q4.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+char * convertNumberIntoArray( int number);
+
+int main(void) {
+    int first =100,second=100;
+    for(first;first<=999;first++) {
+
+        for(second;second<=999;second++) {
+            int product = first*second;
+
+
+
+
+        }
+    }
+
+
+    return 0;
+}

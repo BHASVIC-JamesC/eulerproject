63105204eeef1a130dbfb942d835fdb1d06e16cb
JamesCook5 <james.cook24@students.bhasvic.ac.uk>
2024-10-24 13:24:35 +0100

    on Q4

--- Q4.c
+++ Q4.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
-
+#include <string.h>
 char * convertNumberIntoArray( int number);
 
 int main(void) {
-    int first =100,second=100;
-    for(first;first<=999;first++) {
-
-        for(second;second<=999;second++) {
-            int product = first*second;
-
+    setbuf(stdout, 0);
+    int first =900,second=100;
+    for(int i =first ;i<=999;i++) {
+        for(int j =second;j <=999;j++) {
+            int product = i*j;
+            char integer[7];
+            char reverse[7];
+            sprintf(integer,"%d",product);
+            fflush(stdin);
+            strcpy(reverse,integer);
+            puts(integer);
+            puts(reverse);
 
 
 

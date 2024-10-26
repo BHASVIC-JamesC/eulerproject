9c201f4b62e758e01282792c92898ef26fa63a8e
JamesCook5 <james.cook24@students.bhasvic.ac.uk>
2024-10-26 19:12:33 +0100

    started Q16

---
+++ Q16.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+int main(void) {
+
+    char number[2]= "8",number2[3]="16",number3[3]="32";
+    for(int i = 0;i<3;i++) {
+        printf("%d\n%d\n%d",number[i],number2[i],number3[i]);}
+
+
+
+
+    return 0;
+}

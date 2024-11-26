003eb9020fb1f2399b30507ff175da4c7aade22b
JamesCook5 <james.cook24@students.bhasvic.ac.uk>
2024-11-26 22:47:28 +0000

    yes

--- ritangleTriangle2.c
+++ ritangleTriangle2.c
@@ -27,11 +27,16 @@ int co_prime(int num1,int num2) {
 int main(void) {
 
     for(int a = 10;a<100;a++) {
-        for(int b = 10;b<1000;b++) {
-            for(int c = 10;c<1000;c++) {
+        for(int b = 100;b<1000;b++) {
+            for(int c = 100;c<1000;c++) {
                 if(co_prime(a,b)==1 && co_prime(a,c) == 1 && co_prime(b,c)== 1) {
                     if(a*a + b*b == c*c) {
-                        printf("a: %d b: %d c:%d\n",a,b,c);
+                        int digit2b = (b/10) % 10;
+                        int digit3c = c % 10;
+                        if(digit2b == digit3c) {
+                            printf("a: %d b: %d c:%d\n",a,b,c);
+                        }
+
                     }
                 }
             }

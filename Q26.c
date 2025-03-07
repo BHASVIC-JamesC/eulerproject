201ff15b4df42730737216d224e1a72d60d5a6bc
JamesCook5 <james.cook24@students.bhasvic.ac.uk>
2025-03-07 15:02:36 +0000

    yes

--- Q26.c
+++ Q26.c
@@ -3,36 +3,66 @@
 #include <stdbool.h>
 #include <time.h>
 
-int check(int first,int second,int base);
+int periodic(int* decimals);
 
 int main(void) {
-    int count = 0;
-    int decimals[1000] = {0};
-    int place = 0;
-    bool state = true;
-    int first = 10;
-    int second = 7;
-    decimals[place] = first/second;
-    place++;
-    first = (first-second)*10;
-    while(state == true) {
+    int max = 0;
+    int position;
+    for(int i = 2;i<1000;i++) {
+        int decimals[2001] = {0};
+        int place = 0;
+        int first = 10;
+        int second = i;
         decimals[place] = first/second;
-        tempfirst
 
-        for(int j = 1;j<100;j++) {
-            if(decimals[j] == decimals[0]){
-                state = false;
-                break;
-        }
-        }
+        first = (first%second)*10;
+        int x = 0;
+        while(x<2001) {
+            place++;
+            decimals[place] = first/second;
+            first = (first%second)*10;
 
-        count++;
+            x++;
+        }
+        int ans = periodic(decimals);
+        if(ans > max) {
+            max = ans;
+            position = i;
+        }
     }
 
-    printf("%d",count);
-
-
-
+    printf("the value of d:%d",position);
+    printf("\nthe reoccurance cycle:%d",max);
     return 0;
 }
 
+int periodic(int* decimals) {
+    int arr[1001] = {0};
+    arr[0] = decimals[0];
+    int pointer = 0;
+    int length = 1;
+    for(int i = 1;i<2001;i++) {
+        if(length + i >= 2000 ) {
+            return 0;
+        }
+        bool Case = true;
+        for(int j = 0;j<= length;j++) {
+            //check if the decimals index i is the same as the sequence in arr
+            if(decimals[i+j] == arr[j]) {
+                Case = true;
+
+            }
+            else {
+                Case = false;
+                length ++;
+                arr[i] = decimals[i];
+                break;
+            }
+        }
+        if(Case == true) {
+            if(arr[0] == decimals[i+length+1]) {
+                return length;
+            }
+        }
+    }
+}

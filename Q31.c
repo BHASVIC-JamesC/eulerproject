b87773102bd4df03fd6a2fe723a30535c58e5525
JamesCook5 <james.cook24@students.bhasvic.ac.uk>
2025-03-18 17:08:07 +0000

    yep

--- Q31.c
+++ Q31.c
@@ -3,21 +3,21 @@
 #include <stdbool.h>
 #include <time.h>
 
-int count(int* coins,int sum);
+int count(int sum);
 
+int coins[8] = {1,2,5,10,20,50,100,200};
 int main(void) {
 
-    int coins[8] = {1,2,5,10,20,50,100,200};
-
-    printf("%d",count(8,200));
+    printf("%d",count(200));
 
     return 0;
 }
 
 
-int count(int* coins,int sum) {
-    int max = sizeof(coins)/sizeof(coins[0]);
-    int dp[max][sum];
+int count(int sum) {
+    int max = sizeof(coins)/ sizeof(coins[0]);
+
+    int dp[max+1][sum+1];
     for(int i = 0; i< max;i++) {
         for(int j = 0; j< sum;j++) {
             dp[i][j] = 0;
@@ -25,9 +25,13 @@ int count(int* coins,int sum) {
     }
 
     dp[0][0] = 1;
-    for(int i = 0; i < max;i++) {
-        for(int j =1;j < sum;j++) {
-            dp[i][j+1] = dp[i][j-1] + (dp[i][j] - dp[i-1][j-1]);
+    for(int i = 1; i <= max;i++) {
+        for(int j =0;j <= sum;j++) {
+            dp[i][j] = dp[i-1][j];
+            if(j-coins[i-1]>= 0) {
+                dp[i][j] = dp[i][j] + dp[i][j-coins[i-1]];
+            }
+
         }
     }
     return dp[max][sum];

2a38e0cc2895bfc839582f7f5291ff00bcb41469
JamesCook5 <james.cook24@students.bhasvic.ac.uk>
2025-03-18 15:54:44 +0000

    yes

--- Q31.c
+++ Q31.c
@@ -3,11 +3,32 @@
 #include <stdbool.h>
 #include <time.h>
 
+int count(int* coins,int sum);
 
 int main(void) {
 
     int coins[8] = {1,2,5,10,20,50,100,200};
 
+    printf("%d",count(8,200));
 
     return 0;
+}
+
+
+int count(int* coins,int sum) {
+    int max = sizeof(coins)/sizeof(coins[0]);
+    int dp[max][sum];
+    for(int i = 0; i< max;i++) {
+        for(int j = 0; j< sum;j++) {
+            dp[i][j] = 0;
+        }
+    }
+
+    dp[0][0] = 1;
+    for(int i = 0; i < max;i++) {
+        for(int j =1;j < sum;j++) {
+            dp[i][j+1] = dp[i][j-1] + (dp[i][j] - dp[i-1][j-1]);
+        }
+    }
+    return dp[max][sum];
 }

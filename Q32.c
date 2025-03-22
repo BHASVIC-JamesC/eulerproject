c0ae136751e3f31030400b0cbda58bb180671e04
JamesCook5 <james.cook24@students.bhasvic.ac.uk>
2025-03-22 14:52:08 +0000

    yep

--- Q32.c
+++ Q32.c
@@ -3,39 +3,66 @@
 #include <stdbool.h>
 #include <time.h>
 
-void set(int a,int b, int c);
+bool set(int* ourdigit);
 
 int main(void) {
-    int total[1000] = {0};
+    int total[10] = {0};
     int pointer = 0;
     int totalsum = 0;
-    for(int a = 11;a<=99;a++) {
-        for(int b = 100;b<=999;b++) {
+    for(int a = 1;a<=9;a++) {
+        for(int b = 1000;b<=9999;b++) {
             int c = a*b;
-            if(c > 1000) continue;
+            if(c < 1000 || c>10000) continue;
+            int digit1a = a%10;
+            //int digit2a = a%10;
+            int digit1b = b/1000;
+            int digit2b = b/100 % 10;
+            int digit3b = b /10 %10;
+            int digit4b = b% 10;
+            int digit1c = c/1000;
+            int digit2c = c/100 %10;
+            int digit3c = c/10 %10;
+            int digit4c = c % 10;
+            ;
+            int ourdigit[9] = {digit1a,digit1b,digit2b,digit3b,digit4b,digit1c,digit2c,digit3c,digit4c};
+            if(set(ourdigit) == true) {
+                bool pass = true;
+                for(int i = 0;i<10;i++) {
+                    if(c == total[i]) {
+                        pass = false;
+                    }
+                }
+                if(pass == true) {
+                    total[pointer] = c;
+                    printf("%d x %d = %d\n",a,b,total[pointer]);
+                    pointer ++;
+                    totalsum = totalsum + c;
+                }
 
-
+            }
 
 
         }
     }
 
-    for(int i = 0;i<1000;i++) {
-        printf("%d\n",total[i]);
-    }
+    printf("total sum:%d",totalsum);
     return 0;
 }
 
-void set(int a,int b,int c) {
-    int digit1a = a/10;
-    int digit2a = a%10;
-    int digit1b = b/100;
-    int digit2b = b/10 % 10;
-    int digit3b = b %10;
-    int digit1c = c/1000;
-    int digit2c = c/100 %10;
-    int digit3c = c/10 %10;
-    int digit4c = c % 10;
+bool set(int* arr) {
+
+    for(int i = 0;i<8;i++) {
+        for(int j = i+1;j<9;j++) {
+
+            if(arr[i] == arr[j] || arr[i] == 0 || arr[j] == 0) {
+                return false;
+
+            }
+        }
+    }
+
+    return true;
 
 
 }
+
